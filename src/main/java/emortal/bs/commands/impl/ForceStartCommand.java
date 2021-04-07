dacc41b69488904f38c9c3b8281042e342e0f9f7
Luis <luisjk266@gmail.com>
2021-04-07 19:14:29 +0100

    Made ForceStartCommand work again

--- src/main/java/emortal/bs/commands/impl/ForceStartCommand.java
+++ src/main/java/emortal/bs/commands/impl/ForceStartCommand.java
@@ -16,7 +16,7 @@ public class ForceStartCommand extends Command {
     public boolean execute(CommandSender commandSender, String s, String[] strings) {
         if (commandSender.getName().equalsIgnoreCase("SuperLegoLuis") || commandSender.getName().equalsIgnoreCase("emortl")) {
             Game game = Main.gameMap.get(commandSender);
-            if (game == null || !game.getState().equals(GameState.STARTING)) return true;
+            if (game == null || !(game.getState().equals(GameState.WAITING) || game.getState().equals(GameState.STARTING))) return true;
             if (game.gameStartTask != null) game.gameStartTask.cancel();
             game.start();
         }
